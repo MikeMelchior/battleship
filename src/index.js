9433fb657027081146c072dd15f186ecc3600183
Mike Melchior <mike.melchior@outlook.com>
2023-03-23 15:31:52 -0400

    create ships object full of potential ships for game, create ship factory to keep track of ship and if ship is sunk, being working on gameBoard factory

--- src/index.js
+++ src/index.js
@@ -1,4 +1,112 @@
-export default log = () => {
+let ships = {
+    carrier: {
+        name: 'Carrier',
+        size: 5,
+        code: 'Ca',
+    },
+    battleship: {
+        name: 'Battleship',
+        size: 4,
+        code: 'Ba',
+    },
+    cruiser: {
+        name: 'Cruiser',
+        size: 3,
+        code: 'Cr',
+    },
+    submarine: {
+        name: 'Submarine',
+        size: 3,
+        code: 'Su',
+    },
+    destroyer: {
+        name: 'Destroyer',
+        size: 2,
+        code: 'De',
+    },
+}
 
-    return 'hello'
+const shipFactory = (name, size) => {
+    let shipName = name;
+    let hits = 0;
+
+    let hit = () => {
+        hits++;
+    }
+
+    return {
+        get shipName() {
+            return shipName;
+        },
+        get hits() {
+            return hits;
+        },
+        hit,
+        get isSunk() {
+            return size === hits;
+        },
+    }
+}
+
+const gameBoard = (name, length) => {
+    let boardName = name;
+
+    let createBoard = () => {
+        let board = [];
+        for (let i = 0; i < length; i++) {
+            let row = [];
+            for (let j = 0; j < length; j++) {
+                row.push(0);
+            }
+            board.push(row);
+        }
+        return board;
+    }
+
+    let board = createBoard(length);
+
+    // 0 For empty square;
+    // 1 for missed square;
+    // 2 for hit
+    // Ca for carrier;
+    // Ba for battleship;
+    // Cr for cruiser;
+    // De for destroyer;
+    // S for submarine;
+
+    let placeShips = (ship, coordOne, coordTwo) => {
+
+        let horizontal = false;
+        if (coordOne[0] == coordTwo[0]) horizontal = true;
+
+        if (horizontal) {
+           for (let i=coordOne[0]; i<coordTwo[0] + ship.size; i++) {
+            console.log(coordOne[0])
+            board[coordOne[0]][coordOne[1]] = 1;
+            coordOne[0]++
+           }
+           console.log(board)
+
+        } else {
+            
+        }
+
+    }
+
+    return { 
+        get board() {
+            return board;
+        },
+        get name() {
+            return boardName;
+        },
+        placeShips
+    }
+}
+
+
+module.exports = {
+    ships,
+    shipFactory,
+    gameBoard,
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,42 @@
-import * as index from './index'
+const index = require('./index')
 
-test('testing works', () => {
-    expect(index.default()).toBe('hello')
+
+// test shipFactory;
+test('returns ship', () => {
+    expect(typeof index.shipFactory()).toEqual('object')
 })
+
+test('isSunk method working correctly', () => {
+    let x = index.shipFactory('', 2);
+    x.hit()
+    expect(x.isSunk).toBe(false)
+    x.hit()
+    expect(x.isSunk).toBe(true);
+})
+
+test('ship uses ship object correctly', () => {
+    let x = index.shipFactory(index.ships.battleship.name,
+                                index.ships.battleship.name);
+    expect(x.shipName).toBe('Battleship')
+})
+
+// test Gameboard 
+
+test('returns board', () => {
+    expect(index.gameBoard('board', 2).board).toEqual([[0, 0], [0, 0]])
+})
+
+test.only('places ship', () => {
+    let playerOne = index.gameBoard('Player 1 Ocean', 4);
+    playerOne.placeShips(index.ships.submarine, [4, 0], [4, 2])
+
+    expect(playerOne.board).toEqual([
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 0]
+    ])
+})
+
+
+
