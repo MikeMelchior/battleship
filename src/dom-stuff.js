c0f95916244207cea4561905ed3307965066475d
Mike Melchior <mike.melchior@outlook.com>
2023-03-27 13:58:22 -0400

    add header, adjust createGameBoard function, begin newGameWindow function, create getSquaresCoordinates function, create highlightShips function

--- src/dom-stuff.js
+++ src/dom-stuff.js
@@ -1,8 +1,13 @@
 const index = require('./index')
-const gameLoop = require('./game-loop')
-// require('./styles.css');
 
 import './styles.css'
+
+const header = document.createElement('header');
+header.className = 'header';
+const headTitle = document.createElement('p');
+headTitle.textContent = 'Battleship';
+header.append(headTitle)
+document.body.appendChild(header)
 
 const main = document.createElement('div');
 main.className = 'main'
@@ -13,11 +18,25 @@ gameArea.className = 'game-area';
 main.append(gameArea)
 
 
+const newGameWindow = () => {
+    // enter name
+    // place ships
+    // reset button
+    // start button
+}
 
-const createGameBoard = (board) => {
+
+const createGameBoard = (board, infoString) => {
     let element = document.createElement('div');
-    element.className = 'game-board';
+    element.className = `game-board ${infoString.split(' ').join('-')}`;
 
+      // info / board title
+    let info = document.createElement('p');
+    info.textContent = infoString;
+    element.append(info)
+
+      // use rowCount and squareCount variables to give each square
+      // a 'coord' attribute that corresponds to board
     let rowCount = 0
     for (let row of board) {
         let squareCount = 0;
@@ -25,10 +44,8 @@ const createGameBoard = (board) => {
         gameRow.className = 'row'
         for (let square of row) {
             let gameSquare = document.createElement('div');
-            gameSquare.className = `${rowCount}, ${squareCount}`;
-            gameSquare.addEventListener('click', (e) => {
-                console.log(e.target.className)
-            }) 
+            gameSquare.classList.add('square');
+            gameSquare.setAttribute('coord', `${rowCount}, ${squareCount}`)
             gameRow.append(gameSquare);
             squareCount++
         }
@@ -40,9 +57,32 @@ const createGameBoard = (board) => {
     gameArea.append(element);
 }
 
+const getSquaresCoordinates = (board) => {
+    let squares = document.querySelectorAll(`.${board} > div > div`)
+    squares.forEach(square => {
+        square.addEventListener('click', (e) => {
+            console.log(e.target.attributes[0].nodeValue)
+            return e.target.attributes[0].nodeValue;
+        })
+    })
+}
+
+const highlightShips = (board) => {
+    let squares = document.querySelectorAll('.Player-1-Board .square');
+    squares.forEach(square => {
+        let y = square.attributes[1].nodeValue[0];
+        let x = square.attributes[1].nodeValue[3];
+        if (board[y][x] !== '') {
+            square.style.backgroundColor = 'gray'
+        }    
+    })
+
+}
 
 
 
 export {
-    createGameBoard
+    createGameBoard,
+    highlightShips,
+    getSquaresCoordinates
 }
